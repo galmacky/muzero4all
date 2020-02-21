e50aed45b97e80fa46c43c6f23da5982e0087f1e
Changwan Ryu <galmacky@gmail.com>
2020-02-20 22:42:14 -0800

    Add more structure for muzero dynamics model

--- muzero_dynamics_model.py
+++ muzero_dynamics_model.py
@@ -3,4 +3,22 @@ from mcts_dynamics_model import MctsDynamicsModel
 
 
 class MuZeroDynamicsModel(MctsDynamicsModel):
-    pass
+
+    def __init__(self, env, network):
+        self.env = env
+        self.network = network
+
+    def set_network(self, network):
+        self.network = network
+
+    def get_initial_states(self):
+        return self.network.encode(self.env.get_states())
+
+    def reset(self):
+        # This is called when we call policy.reset()
+        self.network.reset()
+        pass
+
+    def step(self, states, action):
+        # TODO: return (new_states, is_final, immediate_reward, policy_prior_dict, predicted_value_for_new_states)
+        return self.network.virtual_step(states)
