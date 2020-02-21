4878f1aec686366a431120f509a1329b04d8d79b
Changwan Ryu <galmacky@gmail.com>
2020-02-20 20:12:47 -0800

    Fix formatting

--- mcts_env.py
+++ mcts_env.py
@@ -1,4 +1,3 @@
-
 import abc
 
 
@@ -18,7 +17,7 @@ class MctsEnv(object):
 
     @abc.abstractmethod
     def step(self, action):
-    	"""Returns (states, is_final, reward). 
+        """Returns (states, is_final, reward).
     	Note: states will eventually get called bet into set_states()
     	"""
         pass
