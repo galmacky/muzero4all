ddc5ed08fc2098fe8ec02a7f732e583a3ecb55cb
timkim111 <timmykim111@gmail.com>
2020-02-23 16:35:48 -0800

    frozen_lake_det_env_test.py runs now
    Logs:
    
    (.env) timkim-macbookpro2:muzero4all timkim$ python3 frozen_lake_det_env_test.py
    [2020-02-23 16:34:52,206] Making new env: Deterministic-4x4-FrozenLake-v0
    /Users/timkim/Documents/GitHub/muzero4all/.env/lib/python3.6/site-packages/gym/envs/registration.py:17: PkgResourcesDeprecationWarning: Parameters to load are deprecated.  Call .resolve and .require separately.
      result = entry_point.load(False)
    
    SFFF
    FHFH
    FFFH
    HFFG
      (Left)
    SFFF
    FHFH
    FFFH
    HFFG
      (Up)
    SFFF
    FHFH
    FFFH
    HFFG
      (Down)
    SFFF
    FHFH
    FFFH
    HFFG
      (Left)
    SFFF
    FHFH
    FFFH
    HFFG
      (Up)
    SFFF
    FHFH
    FFFH
    HFFG
      (Up)
    SFFF
    FHFH
    FFFH
    HFFG
      (Up)
    SFFF
    FHFH
    FFFH
    HFFG
      (Up)
    SFFF
    FHFH
    FFFH
    HFFG
      (Down)
    SFFF
    FHFH
    FFFH
    HFFG
    (.env) timkim-macbookpro2:muzero4all timkim$

--- frozen_lake_det_env.py
+++ frozen_lake_det_env.py
@@ -1,20 +1,19 @@
 
 import copy
+import gym
 
 from mcts_env import MctsEnv
 
-# #####################3???
-# # coding: utf-8
-# """Defines some frozen lake maps."""
-# from gym.envs.toy_text import frozen_lake, discrete
-# from gym.envs.registration import register
-#
-#
-# register(
-#     id='Deterministic-4x4-FrozenLake-v0',
-#     entry_point='gym.envs.toy_text.frozen_lake:FrozenLakeEnv',
-#     kwargs={'map_name': '4x4',
-#             'is_slippery': False})
+# coding: utf-8
+"""Defines some frozen lake maps."""
+from gym.envs.toy_text import frozen_lake, discrete
+from gym.envs.registration import register
+
+register(
+    id='Deterministic-4x4-FrozenLake-v0',
+    entry_point='gym.envs.toy_text.frozen_lake:FrozenLakeEnv',
+    kwargs={'map_name': '4x4',
+            'is_slippery': False})
 
 # Deterministic Frozen Lake
 class FrozenLakeEnv(MctsEnv):
@@ -25,7 +24,7 @@ class FrozenLakeEnv(MctsEnv):
         self.default_policy_prior = {k: 1. for k in range(9)}
 
         # 4x4, deterministic due to is_slipper=False
-        self.env = gym.make("Deterministic-v0", is_slippery=False)
+        self.env = gym.make("Deterministic-4x4-FrozenLake-v0")
         seed = 1234
         self.env.seed(seed)
 
@@ -51,7 +50,7 @@ class FrozenLakeEnv(MctsEnv):
     # Take a step and append to the list of actions stored in self._states
     def step(self, action):
         # ob is unused
-        ob, rew, done , _ = env.step(a)
+        ob, rew, done , _ = self.env.step(action)
 
         self._states.append(action)
         new_states = self._states
