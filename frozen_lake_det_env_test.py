bcccd6307e2f0c67e7df5f91bbd2a4f9c1911f4c
timkim111 <timmykim111@gmail.com>
2020-02-25 20:53:21 -0800

    updates FL test

--- frozen_lake_det_env_test.py
+++ frozen_lake_det_env_test.py
@@ -3,6 +3,7 @@
 import gym
 
 from frozen_lake_det_env import FrozenLakeEnv
+from gym.envs.toy_text import frozen_lake, discrete
 
 fl_env = FrozenLakeEnv()
 env = fl_env.env
@@ -10,5 +11,16 @@ for _ in range(10):
 	env.render()
 	# Take random action.
 	fl_env.step(env.action_space.sample())
+
+print("Set State: ")
+fl_env.reset()
+actions = [
+	frozen_lake.RIGHT,
+	frozen_lake.RIGHT,
+	frozen_lake.DOWN,
+]
+
+fl_env.set_states(actions)
+env.render()
 
 env.close()
