79c0038c9665998e940bc65fa173a1f982f38c1d
Filip Juristovski <fjuristovski@gmail.com>
2020-03-09 22:11:51 -0700

    Update mcts model

---
+++ muzero_mcts_model_test.py
@@ -0,0 +1,17 @@
+
+import unittest
+from network_initializer import TicTacToeInitializer
+from network import Network
+from muzero_mcts_model import MuZeroMctsModel
+
+class MuZeroMctsModelTest(unittest.TestCase):
+
+    def setUp(self):
+        pass
+        # self.network_initializer = TicTacToeInitializer()
+        # self.network = Network(self.network_initializer)
+        # self.model = MuZeroMctsModel(env, self.network)
+
+    def test_basic(self):
+        pass
+        
