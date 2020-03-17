95603d3efeb44b342e5eb00f1054d23dfe0faefa
Changwan Ryu <galmacky@gmail.com>
2020-03-16 21:20:30 -0700

    Fix trajectory_test

--- trajectory_test.py
+++ trajectory_test.py
@@ -11,8 +11,8 @@ class TrajectoryTest(unittest.TestCase):
     def test_make_target(self):
         self.trajectory.feed(action=0, reward=0., child_visit_dist={0: 0.1, 1: 0.9}, root_value=0., game_state=[])
         self.trajectory.feed(action=1, reward=1., child_visit_dist={0: 0.2, 1: 0.8}, root_value=0.9, game_state=[])
-        # (value, reward, child_visit_dist)
-        self.assertEqual([(0.9, 0.0, {0: 0.1, 1: 0.9}), (1.0, 1.0, {0: 0.2, 1: 0.8})],
+        # (value, last_reward, child_visit_dist)
+        self.assertEqual([(0.9, 0.0, {0: 0.1, 1: 0.9}), (1.0, 0.0, {0: 0.2, 1: 0.8})],
                          self.trajectory.make_target(state_index=0, num_unroll_steps=1, td_steps=2))
 
 
